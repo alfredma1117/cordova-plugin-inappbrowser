89291409d425d81347d1d4fde81882350232faf0
alfredma1117 <alfredma1117@gmail.com>
2015-05-22 17:39:19 +0800

    Update InAppBrowser.java
    Testing.

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -713,6 +713,8 @@ public class InAppBrowser extends CordovaPlugin {
                 if(openWindowHidden) {
                     dialog.hide();
                 }
+                
+                close.setWidth(close.setHeight());
             }
         };
         this.cordova.getActivity().runOnUiThread(runnable);
